02e63ca214a8edfa95e2a330b5ddb0cdf3100af9
Abhishek Sharan <2019hv92027@wilp.bits-pilani.ac.in>
2020-12-25 12:59:28 +0530

    updated select.js

--- select.js
+++ select.js
@@ -11,8 +11,18 @@ connection.connect();
 // website.com/articles?id=1
 // website.com/articles?id=1; drop table articles;
 
-var id = '1';
+//var id = '1';
+let id = process.argv[2];
 
-var query = connection.query('select * from articles where id = ?', id, function(err, result) {
-  console.log(query.sql);
+let sql = `SELECT * FROM todos WHERE title=` + id ;
+
+connection.query(sql, (error, results, fields) => {
+  if (error) {
+    return console.error(error.message);
+  }
+  console.log(results);
+
+//var query = connection.query('select * from articles where id = ?', id, function(err, result) {
+  //console.log(query.sql);
+  //console.log(query);
 });
