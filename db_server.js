3705fee1f75cac4d6f223f753396ca83bbb0272b
Abhishek Sharan <2019hv92027@wilp.bits-pilani.ac.in>
2020-12-25 10:57:37 +0530

    added db server script

---
+++ db_server.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+   
+    var sql = require("mssql");
+
+    // config for your database
+    var config = {
+        user: 'admin',
+        password: 'G1tRD0n1',
+        server: 'movie-database.c11wvjaboclo.us-east-1.rds.amazonaws.com', 
+        database: 'movies' 
+    };
+
+    // connect to your database
+    sql.connect(config, function (err) {
+    
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+           
+        // query to the database and get the records
+        request.query('select * from movies_tbl', function (err, recordset) {
+            
+            if (err) console.log(err)
+
+            // send records as a response
+            res.send(recordset);
+            
+        });
+    });
+});
+
+var server = app.listen(5000, function () {
+    console.log('Server is running..');
+});
