ea56b256029e6e7f2a7be1ef8c4a9f5518db74e1
Abhishek Sharan <2019hv92027@wilp.bits-pilani.ac.in>
2020-12-25 11:51:42 +0530

    added db server script

--- insert.js
+++ insert.js
@@ -4,6 +4,20 @@ var connection = mysql.createConnection({
   host: 'movie-database.c11wvjaboclo.us-east-1.rds.amazonaws.com',
   user: 'admin',
   password: 'G1tRD0n1',
-  database: 'movie-database' 
+  database: 'mySchema' 
 });
 connection.connect();
+
+var article = {
+  author: 'Abhishek',
+  title: 'test',
+  body: 'assignment'
+};
+
+var query = connection.query('insert into articles set ?', article, function (err, result) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.error(result);
+});
