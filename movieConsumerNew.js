708f6ef05cc852af3544c17424931ece1af30788
Abhishek Sharan <2019hv92027@wilp.bits-pilani.ac.in>
2020-12-29 15:39:38 +0530

    updated

--- movieConsumerNew.js
+++ movieConsumerNew.js
@@ -3,7 +3,7 @@
 import unirest from 'unirest';
 var amqp = require('amqplib/callback_api');
 
-amqp.connect('amqps://sharan316:sharan316316@b-b989224f-410e-4d33-b310-e9d2a0b1df9b.mq.us-east-1.amazonaws.com:5671', function(error0, connection) {
+amqp.connect('amqps://guest:guest@localhost:5671', function(error0, connection) {
     if (error0) {
         throw error0;
     }
@@ -22,21 +22,7 @@ amqp.connect('amqps://sharan316:sharan316316@b-b989224f-410e-4d33-b310-e9d2a0b1d
 
         channel.consume(queue, function(msg) {
             console.log(" Received %s", msg.content.toString());
-			
-			
-			sendRequest = (title) => {
-			const req = unirest("GET", "https://movie-database-imdb-alternative.p.rapidapi.com/");
-
-			req.query({
-			"page": "1",
-			"r": "json",
-			"s": title
 		});
-
-		req.headers({
-		"x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com",
-		"x-rapidapi-key": "YOUR_API_KEY"
-	});
 
 
    req.end(function (res) {
