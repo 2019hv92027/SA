bf90611e83213dbdd2c535693d121e409b570bff
Abhishek Sharan <2019hv92027@wilp.bits-pilani.ac.in>
2020-12-29 16:15:33 +0530

    updated

--- movieProducer.js
+++ movieProducer.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
 var amqp = require('amqplib/callback_api');
-var exchange = 'movies';
+var exchange = 'movietest';
 
-amqp.connect('amqps://sharan316:sharan316316@b-b989224f-410e-4d33-b310-e9d2a0b1df9b.mq.us-east-1.amazonaws.com:5671', function(error0, connection) {
+amqp.connect('amqps://sharan316:sharan316316@b-44612873-26b4-4b16-988e-b4db0e96abba.mq.us-east-1.amazonaws.com:5671', function(error0, connection) {
   if (error0) {
     throw error0;
   }
